2e7a993b5b82261ee3f5b6d87091090d2d8b22d7
Gunnar Sundberg <gunnarsundberg@pm.me>
2021-10-08 15:40:39 +0000

    add tester for getpinfo()

---
+++ xv6_patched/user/readingpstat.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "pstat.h"
+
+// TODO: Register in Makefile
+int main(int argc, char *argv[])
+{
+    //TODO: Implement test function for reading pstat table
+    exit();
+}
