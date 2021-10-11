140d3cd4abb48df5ae053ec902884c8c13e7b0b1
Christian Rodriguez <heathazedaze@yahoo.com>
2021-10-11 19:15:54 -0400

    Changes

--- xv6_patched/user/readingpstat.c
+++ xv6_patched/user/readingpstat.c
@@ -7,5 +7,14 @@
 int main(int argc, char *argv[])
 {
     //TODO: Implement test function for reading pstat table
+	int i;
+	struct pstat table;
+	getpinfo(&table);
+	for (i = 0; i < 25; i++)
+	{
+	//if (!table.inuse[i])
+	//continue;
+	printf(1, "Use: %d   Tickets: %d   PID: %d   Ticks: %d\n", table.inuse[i], table.tickets[i], table.pid[i], table.ticks[i]);
+	}
     exit();
 }
