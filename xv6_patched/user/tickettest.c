f032cbd734b3eb8a768549bc34f6c0aced2903ee
Gunnar Sundberg <gunnarsundberg@pm.me>
2021-10-13 02:05:45 +0000

    add tickettest tester

--- xv6_patched/user/tickettest.c
+++ xv6_patched/user/tickettest.c
@@ -7,5 +7,18 @@
 int main(int argc, char *argv[])
 {
     //TODO: Implement ticket test
+    int i;
+    int rc;
+    printf(1, "Parent PID: %d\n", getpid());
+    for (i = 1; i <= 5; i++){
+        rc = fork();
+        if (rc == 0)
+        {
+            settickets(i * 10);
+            printf(1, "Child %d PID: %d\n", i, getpid());
+            for(;;){}
+            //exit();
+        }
+    }
     exit();
 }
